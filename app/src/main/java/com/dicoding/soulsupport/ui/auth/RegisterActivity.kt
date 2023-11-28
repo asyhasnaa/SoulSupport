43e40fd4d3534c74e5f8806a6c39382d87a95a59
DediMurphy <99854509+DediMurphy@users.noreply.github.com>
2023-11-28 21:36:43 +0700

    halaman login halaman register halaman welcome

---
+++ app/src/main/java/com/dicoding/soulsupport/ui/auth/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.soulsupport.ui.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.soulsupport.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
