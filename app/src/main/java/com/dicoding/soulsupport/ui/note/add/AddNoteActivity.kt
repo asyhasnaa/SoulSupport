c98363277c2676910e61b0ef11cddd550839bdc6
DediMurphy <99854509+DediMurphy@users.noreply.github.com>
2023-12-01 07:30:53 +0700

    perbaikann halaman chat halaman note baru article activty

---
+++ app/src/main/java/com/dicoding/soulsupport/ui/note/add/AddNoteActivity.kt
@@ -0,0 +1,16 @@
+package com.dicoding.soulsupport.ui.note.add
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.soulsupport.databinding.ActivityAddNoteBinding
+
+class AddNoteActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddNoteBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAddNoteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
---
+++ app/src/main/java/com/dicoding/soulsupport/ui/note/detail/DetailNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.soulsupport.ui.note.detail
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.soulsupport.R
+
+class DetailNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_note)
+    }
+}
