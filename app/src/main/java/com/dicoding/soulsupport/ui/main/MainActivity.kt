709c096e624dc1308a6f796b8a542e2001763fa0
asyhasnaa <asyhasnaa03@gmail.com>
2023-12-02 20:38:13 +0700

    perubahan pada halaman Menu

--- app/src/main/java/com/dicoding/soulsupport/ui/main/MainActivity.kt
+++ app/src/main/java/com/dicoding/soulsupport/ui/main/MainActivity.kt
@@ -2,17 +2,17 @@ package com.dicoding.soulsupport.ui.main
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.cardview.widget.CardView
 import com.dicoding.soulsupport.R
 import com.dicoding.soulsupport.databinding.ActivityMainBinding
 import com.dicoding.soulsupport.ui.article.ArticleActivity
-import com.dicoding.soulsupport.ui.auth.welcome.WelcomeActivity
 import com.dicoding.soulsupport.ui.chat.ChatActivity
 import com.dicoding.soulsupport.ui.meditation.MeditationActivity
+import com.dicoding.soulsupport.ui.note.add.AddNoteActivity
 import com.dicoding.soulsupport.ui.note.note.NoteActivity
 import com.dicoding.soulsupport.ui.profile.ProfileActivity
-import com.google.android.material.bottomappbar.BottomAppBar
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -29,18 +29,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    @Suppress("DEPRECATION")
     private fun bottomNavigation() {
-        val bottomAppBar = findViewById<BottomAppBar>(R.id.bottomAppBar)
         val bottomNavigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
 
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
-                R.id.bottom_home -> {
-                    startActivity(Intent(this@MainActivity, MainActivity::class.java))
-                }
                 R.id.bottom_note -> {
-                    startActivity(Intent(this@MainActivity, NoteActivity::class.java))
+                    startActivity(Intent(this@MainActivity, AddNoteActivity::class.java))
                 }
+
                 R.id.bottom_profile -> {
                     startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
                 }
@@ -50,11 +48,15 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun clickListener() {
+        val profile = findViewById<ImageView>(R.id.iv_profile)
         val menuChat = findViewById<CardView>(R.id.Menu1)
         val menuNote = findViewById<CardView>(R.id.Menu2)
         val menuMeditation = findViewById<CardView>(R.id.Menu3)
         val menuArticle = findViewById<CardView>(R.id.Menu4)
 
+        profile.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ProfileActivity::class.java))
+        }
         menuChat.setOnClickListener {
             startActivity(Intent(this@MainActivity, ChatActivity::class.java))
         }
